046c80841dfb9229b09e6e4681e964ed9a9dde9c
scyehan <scyehan@163.com>
2012-12-06 10:48:48 +0800

    finish vesion 0.2

--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -4,98 +4,56 @@ module.exports = function(app) {
 
 var Handler = function(app) {
 	this.app = app;
+	//console.log('connector','server id:'+this.app.get('serverId'));
 };
 
 var handler = Handler.prototype;
 
-/**
- * New client entry chess server. 
- * 
- * @param  {Object}   msg     request message
- * @param  {Object}   session current session object
- * @param  {Function} next    next stemp callback
- * @return {Void}
- */
-handler.create = function(msg, session, next) {
-	var self = this;
-	this.app.rpc.chess.chessRemote.checkRoom(session, msg.room, "host", function(result) {
-		if (result) {
-			console.log('create:before add');
-			self.add(msg, session, 'host', next);
-		} else
-		next(null, {
-			code: 500,
-			msg: "The room is in use!"
-		});
-	});
-};
-
-handler.join = function(msg, session, next) {
-	var self = this;
-	this.app.rpc.chess.chessRemote.checkRoom(session, msg.room, "guest", function(result) {
-		if (result) self.add(msg, session, 'guest', next);
-		else
-		next(null, {
-			code: 500,
-			msg: "The room is in use!"
-		});
-	});
-};
-
-handler.bind = function(msg, session, next) {
+handler.login = function(msg, session, next) {
 	var sessionService = this.app.get('sessionService');
-	var uid = msg.username;
+	var uid = msg.username + '*' + msg.channelId;
+	var sid = this.app.get('serverId');
 	if ( !! sessionService.getByUid(uid)) {
 		next(null, {
 			code: 500,
-			msg: "This user name has already exist!"
+			msg: "This user name has already exist in this channel!"
 		});
 		return;
 	}
 	session.bind(uid);
+	session.set('sid', sid);
+	session.push('sid', function(err) {
+		if (err) {
+			console.error('set player for session service failed! error is : %j', err.stack);
+		}
+	});
+	session.set('name', msg.username);
+	session.push('name', function(err) {
+		if (err) {
+			console.error('set player for session service failed! error is : %j', err.stack);
+		}
+	});
+	session.set('channel', msg.channelId);
+	session.push('channel', function(err) {
+		if (err) {
+			console.error('set channelId for session service failed! error is : %j', err.stack);
+		}
+	});
 	session.on('closed', onUserLeave.bind(null, this.app));
-	next(null, {
-		code: 200,
-		msg: "Login success!"
-	});
-}
-
-handler.add = function(msg, session, role, next) {
-	console.log('add:begin add');
-	var self = this;
-	var rid = msg.room;
-	session.set('rid', rid);
-	session.set('role', role);
-	session.push('rid', function(err) {
-		if (err) {
-			console.error('set rid for session service failed! error is : %j', err.stack);
-		}
-	});
-	session.push('role', function(err) {
-		if (err) {
-			console.error('set role for session service failed! error is : %j', err.stack);
-		}
-	});
-
-	//put user into channel
-	self.app.rpc.chess.chessRemote.add(session, msg.username, self.app.get('serverId'), rid, session.get('role'), function(users) {
+	this.app.rpc.chess.chessRemote.login(session,msg.channelId,msg.username,uid,sid,function(data)
+	{
+		//console.log(data);
 		next(null, {
-			code: 200,
-			users: users
-		});
+		code: 200,
+		msg: "Login success!",
+		rooms:data
+	});
 	});
 }
 
-/**
- * User log out handler
- *
- * @param {Object} app current application
- * @param {Object} session current session object
- *
- */
 var onUserLeave = function(app, session) {
 	if (!session || !session.uid) {
 		return;
 	}
-	app.rpc.chess.chessRemote.kick(session, session.uid, app.get('serverId'), session.get('rid'), session.get('role'), null);
+	app.rpc.chess.chessRemote.kick(session, session.get('channel'),session.uid);
 };
